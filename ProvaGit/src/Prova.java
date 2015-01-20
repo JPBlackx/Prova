5295826f2791b7029ff53b61a73b18d235d98571
josep <josep@josep-1225B>
2015-01-20 20:05:21 +0100

    prova

---
+++ ProvaGit/src/Prova.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author josep
+ *
+ */
+public class Prova {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
