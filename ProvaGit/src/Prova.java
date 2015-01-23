f0aa5132df882f7f500096a07a5d765df8016237
JoanLikes <JoanLikes@192.168.7.155>
2015-01-23 16:25:25 +0100

    g

--- ProvaGit/src/Prova.java
+++ ProvaGit/src/Prova.java
@@ -13,8 +13,8 @@ public class Prova {
 	 */
 	public static void main(String[] args) {
 		System.out.print("joan ets un guapo");
-		System.out.print("jtu mes");
-
+		System.out.print("ccaaaaaaaaaaagondeu");
+ 
 	}
 
 }
